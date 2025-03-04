91b66abb009f3fafb05dd613d768e7442dcf5a29
Anna-Essen <anna.nordberg@esseninternational.com>
2025-03-04 12:16:23 +0100

    Map structure, style structure and functionality

--- FrontendChallenge-master/src/app/app.component.ts
+++ FrontendChallenge-master/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CountdownComponent } from './countdown/countdown.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, RouterModule, CountdownComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'FrontendChallenge'
+export class AppComponent implements OnInit {
+  ngOnInit() {
+  }
 }
---
+++ FrontendChallenge-master/src/app/countdown/countdown.component.ts
@@ -0,0 +1,118 @@
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, HostListener, ChangeDetectorRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-countdown',
+  standalone: true,
+  imports: [FormsModule, RouterModule],
+  templateUrl: './countdown.component.html',
+  styleUrls: ['./countdown.component.scss'],
+})
+export class CountdownComponent implements OnInit, AfterViewInit {
+  title: string = '';
+  date: string = '';
+  countdown: string = '';
+  countdownInterval: any;
+
+  @ViewChild('titleElement') titleElement!: ElementRef;
+  @ViewChild('countdownElement') countdownElement!: ElementRef;
+  @ViewChild('countdownContainer') countdownContainer!: ElementRef;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.loadFromLocalStorage();
+    if (this.date && !isNaN(new Date(this.date).getTime())) {
+      this.updateCountdown();
+      this.restartCountdown();
+    } else {
+      this.countdown = 'Please set a valid date to start the countdown.';
+    }
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.adjustFontSize(this.titleElement.nativeElement);
+      this.adjustFontSize(this.countdownElement.nativeElement);
+    }, 100);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.adjustFontSize(this.titleElement.nativeElement);
+    this.adjustFontSize(this.countdownElement.nativeElement);
+  }
+
+  updateCountdown() {
+    if (this.date && !isNaN(new Date(this.date).getTime())) {
+      const targetDate = new Date(this.date).getTime();
+      const now = new Date().getTime();
+      const distance = targetDate - now;
+
+      if (distance > 0) {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        this.countdown = `${days} days, ${hours} h, ${minutes}m, ${seconds}s`;
+      } else {
+        this.countdown = 'The date has passed, please enter a new date.';
+      }
+    } else {
+      this.countdown = 'Please set a valid date to start the countdown.';
+    }
+    this.adjustFontSize(this.countdownElement.nativeElement);
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('title', this.title);
+    localStorage.setItem('date', this.date);
+  }
+
+  loadFromLocalStorage() {
+    this.title = localStorage.getItem('title') || '';
+    this.date = localStorage.getItem('date') || '';
+  }
+
+  adjustFontSize(element: HTMLElement) {
+    const containerWidth = this.countdownContainer.nativeElement.clientWidth - 48;
+    let fontSize = 10;
+    element.style.fontSize = `${fontSize}px`;
+    while (element.scrollWidth <= containerWidth && fontSize < 200) {
+      fontSize++;
+      element.style.fontSize = `${fontSize}px`;
+    }
+    element.style.fontSize = `${fontSize - 1}px`;
+    this.cdr.detectChanges();
+  }
+
+  onTitleChange() {
+    this.saveToLocalStorage();
+    this.cdr.detectChanges();
+    setTimeout(() => {
+      this.adjustFontSize(this.titleElement.nativeElement);
+    }, 0);
+  }
+
+  onDateChange() {
+    this.saveToLocalStorage();
+    this.cdr.detectChanges();
+    this.updateCountdown();
+    this.restartCountdown();
+    this.adjustFontSize(this.countdownElement.nativeElement);
+    setTimeout(() => {
+      this.adjustFontSize(this.countdownElement.nativeElement);
+    }, 0);
+  }
+
+  restartCountdown() {
+    if (this.countdownInterval) {
+      clearInterval(this.countdownInterval);
+    }
+    this.countdownInterval = setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
+  }
+}
